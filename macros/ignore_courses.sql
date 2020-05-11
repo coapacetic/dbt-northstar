fb83f99116ef7ae81b8647cf4c52aecbaaa3a0b1
coapacetic <kyle.d.coapman@gmail.com>
2020-05-11 15:25:45 -0400

    Updated Ignored Classes

--- macros/ignore_courses.sql
+++ macros/ignore_courses.sql
@@ -1,6 +1,6 @@
 {% macro ignore_courses(table_name) %}
 
-{% set ignore_courses = ['ADV100', "ADV500", "DEAN01", "ENG110F", "SH100", "SR101", "SR201", "SR301", "SR401", "TEAM9", "UNI251", "UNI252"] %}
+{% set ignore_courses = ['ADV100', "ADV500", "DEAN01", "ENG110F", "SH100", "SR101", "SR201", "SR301", "SR401", "TEAM9", "UNI251", "UNI252", "UNI605", "UNI606", "UNI601", "UNI600", "UNI603"] %}
 
 where {% for course in ignore_courses %} {{ table_name }}.course_id != '{{ course }}' {% if not loop.last %} AND {% endif %} {% endfor %} 
 
